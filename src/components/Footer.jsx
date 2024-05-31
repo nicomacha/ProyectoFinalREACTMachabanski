92fb5a8a288050d9459b71e3852e9a438ad47f9a
Nicolas Machabanski <nicomachaa@gmail.com>
2024-05-30 22:41:25 -0300

    terminando

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,15 @@
-import truck from "../assets/images/truck.svg";
-import card from "../assets/images/card.svg";
 const Footer = () => {
   return (
-    <div className="container-fluid bg-black">
-      <div className="row mt-3 text-center">
-        <div className="col color">
-          <img src={truck} alt="" />
+    <div className="container-fluid bg-black ">
+      <div className="row text-center">
+        <div className="col text-white">
           <h5>ENVIOS A TODO EL PAIS</h5>
           <p>
             Envios por Correo Argentino, luego de la compra se despacha a los
             2/3 dias habiles
           </p>
         </div>
-        <div className="col">
-          <img src={card} alt="" />
+        <div className="col text-white">
           <h5>6 CUOTAS SIN INTERES</h5>
           <p>Con tarjetas VISA o MASTERCARD</p>
         </div>
